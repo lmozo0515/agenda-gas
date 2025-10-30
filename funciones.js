a42587ef54490f497429283585946ffbc1ee4c33
Lmozo0515 <lmozo0515@gmail.com>
2025-10-30 14:42:47 -0500

    video9 curso gas-avanzado v2

--- funciones.js
+++ funciones.js
@@ -9,3 +9,11 @@ function obtenerDatosHTML(nombre)
   return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 
 }
+
+function obtenerContactos()
+{
+
+  let hoja = SpreadsheetApp.openById('19VK2HhBJhAV0hYhePShvGKDTRxI6L9Hpvl4PCFDk4-Q').getActiveSheet();
+  let datos = hoja.getDataRange().getValues();
+  return datos;
+}
